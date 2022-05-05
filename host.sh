6b041cd701e859bd830e731c946c2d20e548e5ff
Hepheir <hepheir@gmail.com>
2022-05-06 02:27:18 +0900

    refactor(host.sh): use function `get_host`, `get_port`

--- host.sh
+++ host.sh
@@ -7,8 +7,8 @@ function get_port() {
 }
 
 echo "================================================================"
-echo "Current host is: http://$(ifconfig | ggrep "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(?=.+broadcast)" -P -o):4000"
+echo "Current host is: http://$(get_host):$(get_port)"
 echo "================================================================"
 git submodule update --remote
 echo "----------------------------------------------------------------"
-bundle exec jekyll serve --draft --incremental --host="0.0.0.0" --port=4000
+bundle exec jekyll serve --draft --incremental --host="$(get_host)" --port="$(get_port)"
