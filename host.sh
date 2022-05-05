df5ef46ab257012a0b4cbd0cb95c9e10f660b06e
Hepheir <hepheir@gmail.com>
2022-05-06 02:18:48 +0900

    refactor(host.sh): create function `get_port`

--- host.sh
+++ host.sh
@@ -2,6 +2,10 @@ function get_host() {
     echo "$(ifconfig | ggrep "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(?=.+broadcast)" -P -o)"
 }
 
+function get_port() {
+    echo "4000"
+}
+
 echo "================================================================"
 echo "Current host is: http://$(ifconfig | ggrep "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(?=.+broadcast)" -P -o):4000"
 echo "================================================================"
