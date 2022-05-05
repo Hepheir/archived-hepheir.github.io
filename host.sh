93fecaa89163d68de19aaebd1d4fca3f39e6ae1f
Hepheir <hepheir@gmail.com>
2022-05-06 02:21:03 +0900

    refactor(host.sh): remove `--incremental` option

--- host.sh
+++ host.sh
@@ -11,4 +11,4 @@ echo "Current host is: http://$(get_host):$(get_port)"
 echo "================================================================"
 git submodule update --remote
 echo "----------------------------------------------------------------"
-bundle exec jekyll serve --draft --incremental --host="$(get_host)" --port="$(get_port)"
+bundle exec jekyll serve --draft --host="$(get_host)" --port="$(get_port)"
